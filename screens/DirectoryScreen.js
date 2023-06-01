cb86a2d3215db151412524c4a8fa25fb3ce0c526
Tulio Salvatierra <salvacorp@gmail.com>
2023-06-01 10:19:01 -0500

    HOMESCREENADD

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,33 +1,35 @@
-import { Flatlist } from 'react-native';
-import { Avatar, ListItem } from 'react-native-elements';
 import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 
+const DirectoryScreen = ({ navigation }) => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
 
-function DirectoryScreen({ navigation }) {
-  const [campsites, setCampsites] = useState(CAMPSITES);   
-
-
-  const renderDirectoryItem = ({item: campsite}) => {
+    const renderDirectoryItem = ({ item: campsite }) => {
+        return (
+            <ListItem
+                onPress={() =>
+                    navigation.navigate('CampsiteInfo', { campsite })
+                }
+            >
+                <Avatar source={campsite.image} rounded />
+                <ListItem.Content>
+                    <ListItem.Title>{campsite.name}</ListItem.Title>
+                    <ListItem.Subtitle>
+                        {campsite.description}
+                    </ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    };
     return (
-        <ListItem onPress={() => navigation.navigate('CampsiteInfo', { campsite })}>
-            <Avatar source={campsite.image} rounded/>
-            <ListItem.Content>
-                <ListItem.Subtitle>
-                    {campsite.description}
-                </ListItem.Subtitle> 
-            </ListItem.Content>
-        </ListItem>
-    )
-  }  
-
-  return (
-    <Flatlist
-        data={campsites}
-        renderItem={renderDirectoryItem}
-        keyExtractor={(item) => item.id.toString()}
-    />
-  )
-}
+        <FlatList
+            data={campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor={(item) => item.id.toString()}
+        />
+    );
+};
 
 export default DirectoryScreen;
