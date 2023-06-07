53df07b6ed952729d006873a8219dd79f75006d5
Tulio Salvatierra <salvacorp@gmail.com>
2023-06-06 20:16:41 -0500

    last changes

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { ScrollView } from "react-native";
-import { Avatar, Card, ListItem } from "react-native-elements";
+import { Avatar, Card, ListItem, Text } from "react-native-elements";
 import { PARTNERS } from "../shared/partners";
 import { useState } from "react";
 import Mission from "./Mission";
 
 const AboutScreen = () => {
-  const [partner, setPartner] = useState(PARTNERS);
+  const [partners, setPartners] = useState(PARTNERS);
 
   return (
     <ScrollView>
@@ -15,23 +15,25 @@ const AboutScreen = () => {
 
       <Card>
         <Card.Title>Community Partners</Card.Title>
-        <Card.Divider />
-            {PARTNERS.map((partner) => {
-                <ListItem key={partner.id}>
-                    <Avatar rounded source={partner.image} />
-                    <ListItem.Content>
-                    <ListItem.Title>
-                        {partner.name}
-                    </ListItem.Title>
-                    <ListItem.Subtitle>
-                        {partner.description}
-                    </ListItem.Subtitle>
-                    </ListItem.Content>
-                </ListItem>;
-            })}
+         <Card.Divider />
+          <Text>
+          {partners.map((partner) => {
+            <ListItem key={partner.id}>
+              <Avatar rounded source={partner.image} />
+              <ListItem.Content>
+                <ListItem.Title>
+                  {partner.name}
+                </ListItem.Title>
+                <ListItem.Subtitle>
+                  {partner.description}
+                </ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>;
+            })}  
+          </Text>
       </Card>
     </ScrollView>
-  );
+  )
 };
 
 export default AboutScreen;
